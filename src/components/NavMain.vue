c0cf7e3dd7c936c711981cb65018f989147746d8
valtersahakyan <valtersahakyan92@gmail.com>
2025-05-13 11:20:17 +0400

    changes

--- src/components/NavMain.vue
+++ src/components/NavMain.vue
@@ -2,19 +2,20 @@
 import {
   SidebarMenu,
   SidebarMenuButton,
-  SidebarMenuItem,
+  SidebarMenuItem, useSidebar,
 } from '@/components/ui/sidebar';
 
 defineProps({
   items: { type: Array, required: true },
 });
+const {setOpenMobile } = useSidebar();
 </script>
 
 <template>
   <SidebarMenu>
     <SidebarMenuItem v-for="item in items" :key="item.title">
       <SidebarMenuButton as-child :is-active="item.isActive">
-        <router-link :to="item.url">
+        <router-link @click="() => setOpenMobile(false)" :to="item.url">
           <component :is="item.icon" />
           <span>{{ item.title }}</span>
         </router-link>
